23c5b561e2b18e949c834a30cda129d35d8b849a
Vazarch <zarchvassili@gmail.com>
2024-03-12 15:28:58 +0100

    fini ?

--- savedata.js
+++ savedata.js
@@ -1,10 +1,11 @@
 let start_time = new Date().getTime();
-let end_time = new Date().getTime(); ;
-let yes = no = langue = type = color = time = idquestion = String();
+let end_time = new Date().getTime();
 let randomChar = [1,2,3,4,5,6,7,8];
 let mesures = [];
 let mesureX = [];
 let mesureY = [];
+let num_question = 0;
+let interVal;
 let savedata = (data) => {
 
     // Creating a XHR object
@@ -52,109 +53,68 @@ let following = () => {
         mesures[1] =  valY;
     }
 };
-let interVal = setInterval(function () {
-    //if(mesures.length !== 0){console.log(mesures)}
-    mesureX = mesures[0];
-    mesureY = mesures[1];
-},13);
+
 let isHidden = (el) => {
-    var style = window.getComputedStyle(el);
+    let style = window.getComputedStyle(el);
     return (style.display === 'none')
 }
-let repondre = () => {
-  // lancer timer
-    start_time = new Date().getTime();
-    document.getElementById("btn_repondre").hidden = "true";
-    if(getRandomInt(2)==1){
-        document.getElementById("btn_yes").hidden = false;
-        document.getElementById("btn_no").hidden = false;
-    }else {
-        document.getElementById("btn_yes_norder").hidden = false;
-        document.getElementById("btn_no_norder").hidden = false;
-    }
-}
-let normalize = (val, max, min) => {return (val - min) / (max - min);}
-let launchExp = (num_question,reponse) => {
-    var color_param;
-    var reponse = reponse || null;
-    var lang_param,amb_param;
-    num_question++;
-    let randint = getRandomInt(8 - num_question);
-    var random = randomChar[randint];
-    randomChar = randomChar.filter(item => item !== random);
-    if(reponse != null){
-      end_time = new Date().getTime();
-      let elapse = (end_time-start_time);
-      console.log(end_time + " "+ start_time + " ("+(end_time-start_time)+")");
-    }
 
-    document.getElementById("info_p").hidden = true;
-    document.getElementById("formulaire").hidden = true;
-    document.getElementById("btn_launch").hidden = true;
-    document.getElementById("btn_repondre").hidden = false;
-    document.getElementById("btn_yes").hidden = true;
-    document.getElementById("btn_no").hidden = true;
-    document.getElementById("btn_yes_norder").hidden = true;
-    document.getElementById("btn_no_norder").hidden = true;
+let normalize = (val, max, min) => {return (val - min) / (max - min);}
+let launchExp = () => {
+    mesureX.push('start');
+    mesureY.push('start');
+    interVal = setInterval(function () {
+        if(mesures !== 0)console.log(mesures)
+        mesureX.push(mesures[0]);
+        mesureY.push(mesures[1]);
+    },13);
+    num_question++;
+    let random = randomChar[getRandomInt(8 - num_question)];
+    randomChar = randomChar.filter(item => item !== random);
+    start_time = new Date().getTime();
+
     switch(parseInt(random)) {
         case 1:
-            document.getElementById("question_title").textContent = "ROUGE"; 
+            document.getElementById("word").textContent = "ROUGE";
             break;
         case 2: 
-            document.getElementById("question_title").textContent = "BLEU";
+            document.getElementById("word").textContent = "BLEU";
             break;
         case 3:
-            document.getElementById("question_title").textContent = "VERT";
+            document.getElementById("word").textContent = "VERT";
             break;
         case 4:
-            document.getElementById("question_title").textContent = "JAUNE";
+            document.getElementById("word").textContent = "JAUNE";
             break;
         case 5:
-            document.getElementById("question_title").textContent = "dvorak";
+            document.getElementById("word").textContent = "dvorak";
             break;
         case 6:
-            document.getElementById("question_title").textContent = "azerty";
+            document.getElementById("word").textContent = "azerty";
             break;
         case 7: 
-            document.getElementById("question_title").textContent = "qwerty";
+            document.getElementById("word").textContent = "qwerty";
             break;
         case 8: 
-            document.getElementById("question_title").textContent = "bepo";
+            document.getElementById("word").textContent = "bepo";
             break; 
-        default: 
-            console.log(time);
-            const data = document.getElementById("formulaire");
-            savedata ({data:new FormData(data),idquestion:idquestion,yes: yes, no: no,type:type,color:color,time:time});
-            document.getElementById("fin_title").hidden = false;
-            document.getElementById("btn_yes").hidden = true;
-            document.getElementById("btn_no").hidden = true;
-            document.getElementById("btn_yes_norder").hidden = true;
-            document.getElementById("btn_no_norder").hidden = true;
-            document.getElementById("btn_repondre").hidden = true;
-            document.getElementById("question_title").hidden = true;
+        default:
+            clearInterval(interVal)
+            document.getElementById("word").textContent = "";
+            savedata ({mesureX,mesureY});
             break;
-        }
-            var el = document.getElementById('btn_yes'),
-            elClone = el.cloneNode(true);
-            el.parentNode.replaceChild(elClone, el);
-            
-            el = document.getElementById('btn_no'),
-            elClone = el.cloneNode(true);
-            el.parentNode.replaceChild(elClone, el);
-
-            el = document.getElementById('btn_yes_norder'),
-            elClone = el.cloneNode(true);
-            el.parentNode.replaceChild(elClone, el);
-
-            el = document.getElementById('btn_no_norder'),
-            elClone = el.cloneNode(true);
-            el.parentNode.replaceChild(elClone, el);
-            
-            document.getElementById("btn_yes").addEventListener('click', function() {launchExp(num_question,["oui",lang_param,amb_param,color_param]);});
-            document.getElementById("btn_no").addEventListener('click', function() {launchExp(num_question,["non",lang_param,amb_param,color_param]);});
-            document.getElementById("btn_yes_norder").addEventListener('click', function() {launchExp(num_question,["oui",lang_param,amb_param,color_param]);});
-            document.getElementById("btn_no_norder").addEventListener('click', function() {launchExp(num_question,["non",lang_param,amb_param,color_param]);});
+    }
 }
 
+function reponse(reponse) {
+    clearInterval(interVal)
+    end_time = new Date().getTime();
+    elapse = end_time - start_time;
+    mesureX.push(elapse);
+    mesureY.push(elapse);
+    mesureX.push(reponse);
+    mesureY.push(reponse);
+    mesures = []
+}
 window.addEventListener('DOMContentLoaded', following);
 
